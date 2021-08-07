6cca29a12ca21dbbef7a474e2499d72e25c5be05
wontae <beegramin9@gmail.com>
2021-08-07 16:54:47 +0900

    Updates

---
+++ precache-manifest.b0e539fa844cd5efbb57e0d0a5caa4ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2756047774316fee3e2c233fa136cdd5",
+    "url": "/React-NetNinja-FramerMotion/index.html"
+  },
+  {
+    "revision": "034d210daee28ed0d76d",
+    "url": "/React-NetNinja-FramerMotion/static/css/main.088a6197.chunk.css"
+  },
+  {
+    "revision": "0cedb0ce819cd5c6acdc",
+    "url": "/React-NetNinja-FramerMotion/static/js/2.48bb8971.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/React-NetNinja-FramerMotion/static/js/2.48bb8971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "034d210daee28ed0d76d",
+    "url": "/React-NetNinja-FramerMotion/static/js/main.0e308ad4.chunk.js"
+  },
+  {
+    "revision": "41701a844da86fe18653",
+    "url": "/React-NetNinja-FramerMotion/static/js/runtime-main.8400e63e.js"
+  }
+]);
